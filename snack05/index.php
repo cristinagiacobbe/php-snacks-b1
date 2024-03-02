61f5eda55fd6f5fafc3efe9ed88b54759fb64c35
cristinagiacobbe <crigiak@gmail.com>
2024-03-02 09:43:30 +0100

    Fix snack05

--- snack05/index.php
+++ snack05/index.php
@@ -7,10 +7,10 @@ Prendere un paragrafo abbastanza lungo, contenente diverse frasi.
 Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. */
 
 $text = "Quando si aggiungono nuovi elementi ad un array e non si specifica un indice, PHP riprende la numerazione partendo dall'indice numerico più alto. Dunque un nuovo elemento inserito avrà indice pari all'indice più grande incrementato di uno.
-Nel nostro esempio, prima di inserire nuovi elementi, l'indice più alto è 2, quindi l'elemento inserito alla riga 3 avrà indice pari a 3. Alla riga 7 abbiamo invece specificato 25 come indice, dunque un eventuale nuovo elemento inserito avrebbe indice pari a 26.";
+Nel nostro esempio, prima di inserire nuovi elementi, l'indice più alto è 2, quindi l'elemento inserito alla riga 3 avrà indice pari a 3. Alla riga 7 abbiamo invece specificato 25 come indice, dunque un eventuale nuovo elemento inserito avrebbe indice pari a 26";
 
 
-$explodedTex = explode(".", $text);
+$explodedText = explode(".", $text);
 var_dump($explodedText)
 ?>
 <!DOCTYPE html>
